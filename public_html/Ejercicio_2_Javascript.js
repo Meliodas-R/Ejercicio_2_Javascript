4e8d082cb1d36af0b6b78578a09b541689c9df44
usuario <usuario@usuario-PC>
2020-12-21 11:33:40 +0100

    Commit inicial
    ·Corrección de errores:
       *N/A
    
    ·Cambios y mejoras:
       *N/A

---
+++ public_html/Ejercicio_2_Javascript.js
@@ -0,0 +1,7 @@
+var rojo = parseInt(Math.random() * 255);
+var verde = parseInt(Math.random() * 255);
+var azul = parseInt(Math.random() * 255);
+
+document.body.style.backgroundColor = 'rgb(' + rojo + ',' + verde + ',' + azul + ')';
+
+
